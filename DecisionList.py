baa9f3c4ac116ae18094ac9880a3e84b961ceb94
Rowan Roshong <rrroshong@gmail.com>
2022-03-02 21:43:30 -0500

    Start get_score()

--- DecisionList.py
+++ DecisionList.py
@@ -2,10 +2,8 @@
 
 import argparse
 import math
-
 import numpy
-
-from Lexelt import get_data, get_key
+from Lexelt import *
 
 
 class DecisionList(object):
@@ -17,7 +15,21 @@ class DecisionList(object):
         self.min_score = min_score
 
     def get_score(self, feature, label, X, y):
-        ...
+        indices = numpy.where(y == label)
+
+        values = X[indices, 0]
+        print(values)
+        values = values[0]
+
+        summed_values = sum(values)
+        without_feature = len(values) - summed_values
+
+        summed_values += self.alpha
+        without_feature += self.alpha
+
+        score = math.log((summed_values/without_feature), 2)
+
+        return score
 
     def fit(self, X, y):
         ...
@@ -30,11 +42,27 @@ class DecisionList(object):
 
 
 def main(args):
-    train_data = get_data(args.traindata)
-    get_key(args.trainkey, train_data)
+    # train_data = get_data(args.traindata)
+    # get_key(args.trainkey, train_data)
+    #
+    # test_data = get_data(args.testdata)
+    # get_key(args.testkey, test_data)
 
-    test_data = get_data(args.testdata)
-    get_key(args.testkey, test_data)
+    train_fp = open("/data/366/senseval3/train/EnglishLS.train", "r")
+    train_data = get_data(train_fp)
+    train_data.keys()
+    trainkey_fp = open("/data/366/senseval3/train/EnglishLS.train.key", "r")
+    get_key(trainkey_fp, train_data)
+
+    d = DecisionList(alpha=0.1, min_score=5, default_target=5)
+
+    lexelt = train_data['activate.v']
+    this_instance = lexelt.get("activate.v.bnc.00044852")
+    this_instance.make_features()
+    feature_names, X_train = lexelt.get_features()
+    answer_labels, Y_train = lexelt.get_targets()
+
+    print(d.get_score(0, 0, X_train, Y_train))
 
     ...
 
